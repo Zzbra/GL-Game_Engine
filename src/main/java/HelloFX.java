004bf00fd901ecbacd7559eae57f8e8e234567b4
Zbrazz <roux.etienne141@gmail.com>
2020-11-05 14:31:59 +0100

    Design de la scène de menu

--- src/main/java/HelloFX.java
+++ src/main/java/HelloFX.java
@@ -8,11 +8,18 @@ public class HelloFX extends Application {
 
     @Override
     public void start(Stage stage) {
-        String javaVersion = System.getProperty("java.version");
-        String javafxVersion = System.getProperty("javafx.version");
-        Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        Scene scene = new Scene(new StackPane(l), 640, 480);
-        stage.setScene(scene);
+//        String javaVersion = System.getProperty("java.version");
+//        String javafxVersion = System.getProperty("javafx.version");
+//        Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
+//        Scene scene = new Scene(new StackPane(l), 640, 480);
+//        stage.setScene(scene);
+//        stage.show();
+        testMenu(stage);
+    }
+
+    public void testMenu(Stage stage){
+        MenuScene MS = new MenuScene();
+        stage.setScene(MS.getScene());
         stage.show();
     }
 
---
+++ src/main/java/MenuScene.java
@@ -0,0 +1,43 @@
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+import javafx.scene.image.Image;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+
+
+import java.awt.*;
+
+public class MenuScene{
+    private Scene scene;
+
+    public MenuScene(){
+        this.scene = new Scene(new Group());
+        Button startGameButton = new Button("Start Game");
+        Button controlButton = new Button("Controls");
+        Button aboutButton = new Button("About");
+        startGameButton.setStyle(" -fx-pref-height: 28px; -fx-pref-width: 200px;");
+        controlButton.setStyle(" -fx-pref-height: 28px; -fx-pref-width: 200px;");
+        aboutButton.setStyle(" -fx-pref-height: 28px; -fx-pref-width: 200px;");
+        Image img = new Image("PacMan.jpg");
+        ImageView imgView = new ImageView(img);
+        VBox vBox = new VBox(imgView,startGameButton, controlButton, aboutButton);
+        vBox.setAlignment(Pos.BOTTOM_CENTER);
+        vBox.setSpacing(20);
+        vBox.setPadding(new Insets(50,10,50,10));
+        StackPane panel = new StackPane(vBox);
+
+        panel.setBackground(new Background(new BackgroundFill(Color.BLACK, CornerRadii.EMPTY, Insets.EMPTY)));
+
+        this.scene = new Scene(panel, 640, 480);
+        // Ne marche pas, problème de path probablement, a voir
+        //scene.getStylesheets().add("main/java/StyleSheet.css");
+    }
+
+    public Scene getScene() {
+        return scene;
+    }
+}
