f143d56547bcbea67793f3d64b48b11fa4fd00b5
Zbra <roux.etienne141@gmail.com>
2020-11-09 18:16:53 +0100

    Gameloop et déplacement

--- src/main/java/GameScene.java
+++ src/main/java/GameScene.java
@@ -1,22 +1,32 @@
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
+import javafx.scene.paint.ImagePattern;
+import javafx.scene.shape.Rectangle;
 
 public class GameScene extends SceneWrapper {
     public GameScene(){
         super();
         Scene gameScene;
-        Image img = new Image("PacMan.jpg");
-        ImageView imgView = new ImageView(img);
-        VBox vBox = new VBox(imgView);
-        vBox.setAlignment(Pos.CENTER);
-        StackPane panel = new StackPane(vBox);
-        panel.setBackground(new Background(new BackgroundFill(Color.BLACK, CornerRadii.EMPTY, Insets.EMPTY)));
-        gameScene = new Scene(panel, 640, 480);
+        Rectangle rectangle = new Rectangle();
+        Image img = new Image("Pac.png");
+        rectangle.setFill(new ImagePattern(img));
+        rectangle.setX(75.0f);
+        rectangle.setY(75.0f);
+        rectangle.setWidth(150.0f);
+        rectangle.setHeight(150.0f);
+        getComponents().put("Shape", rectangle);
+        // ImageView imgView = new ImageView(img);
+        //VBox vBox = new VBox(imgView);
+        //vBox.setAlignment(Pos.CENTER);
+        Group group = new Group(rectangle);
+        //group.setBackground(new Background(new BackgroundFill(Color.BLACK, CornerRadii.EMPTY, Insets.EMPTY)));
+        gameScene = new Scene(group, 640, 480);
         super.setScene(gameScene);
     }
 }
--- src/main/java/HelloFX.java
+++ src/main/java/HelloFX.java
@@ -1,16 +1,25 @@
 import Systems.GraphicalEngine;
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
+import javafx.util.Duration;
+import javafx.scene.shape.*;
 
 public class HelloFX extends Application {
     private SceneWrapper mainMenuScene;
     private SceneWrapper gameScene;
     private GraphicalEngine graphicEngine;
+    private Timeline gameLoop;
     @Override
     public void start(Stage stage) {
+        final Duration fps = Duration.millis(1000/60);
+        final KeyFrame oneFrame = new KeyFrame(fps, evt -> update());
+
         graphicEngine = new GraphicalEngine(stage);
         mainMenuScene = SceneMaker.MakeMenuScene();
         gameScene = SceneMaker.MakeGameScene();
@@ -19,7 +28,12 @@ public class HelloFX extends Application {
             @Override
             public void handle(ActionEvent e) {
                 graphicEngine.loadScene(gameScene.getScene());
+                gameLoop = new Timeline();
+                gameLoop.getKeyFrames().add(oneFrame);
+                gameLoop.setCycleCount(Animation.INDEFINITE);
+                gameLoop.play();
             }
+
         });
 
         graphicEngine.loadScene(mainMenuScene.getScene());
@@ -33,6 +47,12 @@ public class HelloFX extends Application {
 //        stage.setScene(scene);
 //        stage.show();
        // testMenu(stage);
+    }
+
+    private void update(){
+        System.out.println("frame");
+        Rectangle rect = (Rectangle) gameScene.getComponents().get("Shape");
+        rect.setX(rect.getX()+1);
     }
 
 
