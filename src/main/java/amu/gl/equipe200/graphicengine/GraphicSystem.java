bdd75b126ce607236e659e356f620475178be540
Maxime <maxime.schlegel.83@gmail.com>
2020-11-14 22:33:00 +0100

    Start GameSystem

--- src/main/java/amu/gl/equipe200/graphicengine/GraphicSystem.java
+++ src/main/java/amu/gl/equipe200/graphicengine/GraphicSystem.java
@@ -1,4 +1,23 @@
 package amu.gl.equipe200.graphicengine;
 
-public class GraphicSystem {
+import amu.gl.equipe200.core.Entity;
+import amu.gl.equipe200.core.System;
+
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import java.util.ArrayList;
+
+public class GraphicSystem
+    extends System {
+
+    // The graphic engine hold the stage and cached the current dispkayed scene
+    private Stage stage;
+    private Scene currentScene;
+
+    GraphicSystem (Stage stage) { }
+
+    @Override
+    public void onUpdate(long elapsedTime, ArrayList<Entity> affectedEntity) {
+
+    }
 }
