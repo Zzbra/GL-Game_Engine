19de5ebeb7a29c08d503021db18aba7f9832fcc7
William-Barry-Aix <wiwi.barry@hotmail.fr>
2020-11-19 19:55:54 +0100

    mise en place de la classe abstraite Component

--- src/main/java/amu/gl/equipe200/core/Component/Component.java
+++ src/main/java/amu/gl/equipe200/core/Component/Component.java
@@ -1,8 +1,38 @@
 package amu.gl.equipe200.core.Component;
 
+import amu.gl.equipe200.core.GameWorld;
+import amu.gl.equipe200.entity.BaseEntity;
 import amu.gl.equipe200.system.GraphicalEngine;
-import amu.gl.equipe200.system.PhysicalEngine;
 
 public abstract class Component {
-    public void updateBy(GraphicalEngine engine){};
+    private BaseEntity entity;
+
+    public Component(BaseEntity entity){
+        this.entity = entity;
+    }
+
+    public BaseEntity getEntity(){
+        return this.entity;
+    }
+
+    public double getX() {
+        return entity.getX();
+    }
+
+    public double getY() {
+        return entity.getY();
+    }
+
+    public double getR() {
+        return entity.getR();
+    }
+
+    public GameWorld getGameWorld(){
+        return entity.getGameWorld();
+    }
+
+
+    public void updateBy(GraphicalEngine engine){}
+
+    public abstract void remove();
 }
