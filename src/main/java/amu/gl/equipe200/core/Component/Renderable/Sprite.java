a8d5b0a449f2ec9607475de3232dc1d36787188f
William-Barry-Aix <wiwi.barry@hotmail.fr>
2020-11-19 19:56:37 +0100

    Implémentation des Sprites

--- src/main/java/amu/gl/equipe200/core/Component/Renderable/Sprite.java
+++ src/main/java/amu/gl/equipe200/core/Component/Renderable/Sprite.java
@@ -3,19 +3,45 @@ package amu.gl.equipe200.core.Component.Renderable;
 import amu.gl.equipe200.entity.BaseEntity;
 import amu.gl.equipe200.system.GraphicalEngine;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
-public class View extends Renderable {
+public class Sprite extends Renderable {
     private Image image;
-    public View(BaseEntity entity, String imagePath) {
-        super(entity);
-        image = GraphicalEngine.getImages().get(imagePath);
+    private ImageView imageView;
+
+    public Sprite(BaseEntity entity, String imagePath, String layerName) {
+        super(entity, layerName);
+
+        this.image = GraphicalEngine.getImages().get(imagePath);
+        this.imageView = new ImageView(image);
+        super.addToLayer(imageView);
+        entity.setWidth(image.getWidth());
+        entity.setHeight(image.getWidth());
+
+
     }
 
     public void updateBy(GraphicalEngine engine){
         engine.update(this);
     }
 
-    public Image getImage(){
-        return this.image;
+    @Override
+    public void remove() {
+        super.layer.getChildren().remove(this.imageView);
+    }
+
+    public ImageView getView() {
+        return imageView;
+    }
+
+
+    @Override
+    public double getWidth() {
+        return image.getWidth();
+    }
+
+    @Override
+    public double getHeight() {
+        return image.getHeight();
     }
 }
