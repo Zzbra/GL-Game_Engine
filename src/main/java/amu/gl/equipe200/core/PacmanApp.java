cf5f0466d5b9d438fdb489cd31b1f1873d23c685
William-Barry-Aix <wiwi.barry@hotmail.fr>
2020-11-19 20:07:28 +0100

    Suppression de toutes les références aux image de JavaFX depuis l'application et implémentation des Components

--- src/main/java/amu/gl/equipe200/core/PacmanApp.java
+++ src/main/java/amu/gl/equipe200/core/PacmanApp.java
@@ -1,5 +1,7 @@
 package amu.gl.equipe200.core;
 
+import amu.gl.equipe200.core.Component.Renderable.Renderable;
+import amu.gl.equipe200.core.Component.Renderable.Sprite;
 import amu.gl.equipe200.entity.BaseEntity;
 import amu.gl.equipe200.entity.Enemy;
 import amu.gl.equipe200.entity.Player;
@@ -10,19 +12,10 @@ import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
-import javafx.scene.Group;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
-import javafx.scene.layout.Pane;
-import javafx.scene.paint.Color;
-import javafx.scene.text.Font;
-import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
-import javafx.scene.text.TextBoundsType;
 import javafx.stage.Stage;
 
 import java.util.*;
@@ -72,7 +65,7 @@ public class PacmanApp extends Application {
 
                 // update amu.gl.equipe200.entity in scene
                 // Ici le moteur graphique se charge de réafficher les entitées avec leurs coordonnées actualisées
-                graphicalEngine.updateUI(gameScene.getEntities());
+                graphicalEngine.update(gameScene.getEntities());
 
 
                 // check if amu.gl.equipe200.entity can be removed
@@ -145,15 +138,14 @@ public class PacmanApp extends Application {
     }
 
     private void createPlayers() {
-
-        Image image = graphicalEngine.getImages().get("playerImage");
         // center horizontally, position at 70% vertically
-        double x = (Settings.SCENE_WIDTH - image.getWidth()) / 2.0;
+        double x = Settings.SCENE_WIDTH / 2.0;
         double y = Settings.SCENE_HEIGHT * 0.7;
 
         // create player
-        Player player = new Player(gameScene.getLayers().get("playerfieldLayer"), image, x, y, 0, 0, 0, 0, Settings.PLAYER_SHIP_HEALTH, 0, Settings.PLAYER_SHIP_SPEED);
-
+        Player player = new Player(x, y, 0, 0, 0, 0, Settings.PLAYER_SHIP_HEALTH, 0, Settings.PLAYER_SHIP_SPEED, gameScene);
+        Renderable sprite = new Sprite(player, "playerImage", "playerfieldLayer");
+        player.initShip(sprite);
         // register player
         gameScene.getPlayers().add( player);
         systems.get("Collisions").addEntity(player);
@@ -167,19 +159,18 @@ public class PacmanApp extends Application {
             return;
         }
 
-        // image
-        Image image = graphicalEngine.getImages().get("enemyImage");
-
         // random speed
         double speed = rnd.nextDouble() * 1.0 + 2.0;
 
         // x position range: enemy is always fully inside the screen, no part of it is outside
         // y position: right on top of the view, so that it becomes visible with the next game iteration
-        double x = rnd.nextDouble() * (Settings.SCENE_WIDTH - image.getWidth());
-        double y = -image.getHeight();
+        double x = rnd.nextDouble() * Settings.SCENE_WIDTH;
+        double y = 0;
 
         // create a sprite
-        Enemy enemy = new Enemy( gameScene.getLayers().get("playerfieldLayer"), image, x, y, 0, 0, speed, 0, 1,1);
+        Enemy enemy = new Enemy(x, y, 0, 0, speed, 0, 1,1, gameScene);
+        Renderable sprite = new Sprite(enemy, "enemyImage", "playerfieldLayer");
+        enemy.setY(-sprite.getHeight());
 
         // manage sprite
         gameScene.getEnemies().add( enemy);
@@ -194,7 +185,7 @@ public class PacmanApp extends Application {
             if( sprite.isRemovable()) {
 
                 // remove from layer
-                sprite.removeFromLayer();
+                sprite.getComponent(Renderable.class).remove();
                 systems.get("Collisions").removeEntity(sprite);
 
                 // remove from list
