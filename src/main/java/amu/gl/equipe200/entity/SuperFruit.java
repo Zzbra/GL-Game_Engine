4bcef6f44e913c2e9ee06e1b842897fef75eb534
TontonBal <huyghealexandre7@gmail.com>
2020-11-28 19:13:11 +0100

    Création super fruit v0

---
+++ src/main/java/amu/gl/equipe200/entity/SuperFruit.java
@@ -0,0 +1,12 @@
+package amu.gl.equipe200.entity;
+import amu.gl.equipe200.inputengine.IOInterface;
+import amu.gl.equipe200.physicsengine.PhysicsInterface;
+import amu.gl.equipe200.graphicsengine.RenderableInterface;
+import amu.gl.equipe200.core.Entity;
+import amu.gl.equipe200.core.GameWorld;
+import amu.gl.equipe200.gameworld.Settings;
+/*
+public class SuperFruit extends Entity implements PhysicsInterface, RenderableInterface{
+    private String imageName
+}
+*/
