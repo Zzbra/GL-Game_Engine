2138d0f2a923d719733c053d82e21318270c5ab1
Maxime <maxime.schlegel.83@gmail.com>
2020-12-06 22:27:55 +0100

    Start creating gameworld from map

--- src/main/java/amu/gl/equipe200/graphicsengine/ImagesManager.java
+++ src/main/java/amu/gl/equipe200/graphicsengine/ImagesManager.java
@@ -7,13 +7,10 @@ import java.util.HashMap;
 
 public class ImagesManager {
 
-    static final String PATH = "/ressources/images/";
-
     // cache for the images
     HashMap<String, Image> cachedImage = new HashMap<>();
 
     public Image getImage(String name) {
-        System.out.println(name);
         if (this.cachedImage.containsKey(name)) return cachedImage.get(name);
         Image image = new Image(name);
         this.cachedImage.put(name, image);
--- src/main/java/amu/gl/equipe200/pacman/PacmanApp.java
+++ src/main/java/amu/gl/equipe200/pacman/PacmanApp.java
@@ -19,6 +19,9 @@ public class PacmanApp
     private Pacman pacman;
     private Blinky blinky;
 
+    private int grid_size;
+    private double cell_height, cell_width;
+
     public static void main(String[] args) {
         launch(args);
     }
@@ -28,16 +31,22 @@ public class PacmanApp
         System.out.println("Hello onInit");
         this.pacmanWorld = new GameWorld(Settings.WORLD_WIDTH, Settings.WORLD_HEIGHT);
 
-        int[][] map = createMap("Map1.txt");
+        int[][] map = createMap("Map.txt");
+        blinky.setPacMan(pacman);
+
+
         getIaEngine().loadMap(map);
-        createPlayers();
-        createGhost();
+//        createPlayers();
+//        createGhost();
         loadMainMenu();
     }
 
     @Override
     public void onGameIterBegin(double ellapsedTime) {
-        System.out.println(blinky.getXSpeed() + " " + blinky.getYSpeed());
+        /*** update the entities ***/
+        this.pacman.update(ellapsedTime);
+
+        System.out.println("Blinky Speed: " + blinky.getXSpeed() + " " + blinky.getYSpeed());
     }
     public void onGameIterEnd(long ellapsedTime) { }
 
@@ -46,80 +55,108 @@ public class PacmanApp
 
     public GameWorld getPacmanWorld() { return pacmanWorld; }
 
-    private void createPlayers() {
-        // center horizontally, position at 70% vertically
-        double x = 16 / 6.0;
-        double y = 16 * 0.6;
-
-        pacman = new Pacman();
-        pacman.setX(x);
-        pacman.setY(y);
-        pacman.setWidth(0.8);
-        pacman.setHeight(0.8);
-        pacman.setControls("Z", "S", "Q", "D");
-        pacman.setLayerName("FOREGROUND");
-
-        pacmanWorld.addGraphicsEntity(pacman);
-        pacmanWorld.addPhysicsEntity(pacman);
-        pacmanWorld.addIOEntity(pacman);
-    }
-
-    private void createGhost(){
-        blinky = new Blinky();
-        blinky.setX(6);
-        blinky.setY(6);
-        blinky.setPacMan(pacman);
-        blinky.setWidth(0.8);
-        blinky.setHeight(0.8);
-        blinky.setImageName("images/ghostRed.jpg");
-        blinky.setLayerName("FOREGROUND");
-        pacmanWorld.addGraphicsEntity(blinky);
-        pacmanWorld.addPhysicsEntity(blinky);
-        pacmanWorld.addAIEntity(blinky);
-    }
-
     private int[][] createMap(String mapName){
         int[][] mapGrid = getMapGrid(mapName);
 
-        for (int y = 0; y < 16; y++) {
-            for (int x = 0; x < 16; x++) {
+        for (int y = 0; y < grid_size; y++) {
+            for (int x = 0; x < grid_size; x++) {
                 System.out.printf("%d", mapGrid[y][x]);
-                if(mapGrid[y][x] == 1) {
-                    Block block = new Block();
-                    block.setX(x);
-                    block.setY(y);
-                    block.setWidth(1);
-                    block.setHeight(1);
-                    block.setImageName(("images/Wall.png"));
-                    block.setLayerName("FOREGROUND");
-                    pacmanWorld.addGraphicsEntity(block);
-                    pacmanWorld.addPhysicsEntity(block);
-
-                }
-                if(mapGrid[y][x] == 2){
-                    SuperFruit superFruit = new SuperFruit();
-                    superFruit.setX(x);
-                    superFruit.setY(y);
-                    superFruit.setWidth(0.75);
-                    superFruit.setHeight(0.75);
-                    superFruit.setImageName("images/Fruit_Cherry.png");
-                    superFruit.setLayerName("FOREGROUND");
-                    pacmanWorld.addPhysicsEntity(superFruit);
-                    pacmanWorld.addGraphicsEntity(superFruit);
-                }
-                if(mapGrid[y][x] == 0){
-                    PacGomme pacGomme = new PacGomme();
-                    pacGomme.setX(x+0.3);
-                    pacGomme.setY(y+0.3);
-                    pacGomme.setWidth(0.2);
-                    pacGomme.setHeight(0.2);
-                    pacGomme.setImageName("images/pacGomme.jpg");
-                    pacGomme.setLayerName("BACKGROUND");
-                    pacmanWorld.addPhysicsEntity(pacGomme);
-                    pacmanWorld.addGraphicsEntity(pacGomme);
+                switch (mapGrid[y][x]) {
+                    case 0: break; // Blank space
+                    case 1: {
+                        double wall_scale = 1;
+                        double width = cell_width * wall_scale;
+                        double height = cell_height * wall_scale;
+                        Block wall = new Block();
+                        wall.setX((x * cell_width) + (cell_width / 2) - (width / 2));
+                        wall.setY((y * cell_height) + (cell_height / 2) - (height / 2));
+                        wall.setWidth(width);
+                        wall.setHeight(height);
+                        wall.setImageName(("images/Wall.png"));
+                        wall.setLayerName("BACKGROUND");
+                        pacmanWorld.addGraphicsEntity(wall);
+                        pacmanWorld.addPhysicsEntity(wall);
+                        break;
+                    }
+                    case 2: {
+                        double pacgum_scale = 0.25;
+                        double width = cell_width * pacgum_scale;
+                        double height = cell_height * pacgum_scale;
+                        PacGomme PacGomme = new PacGomme();
+                        PacGomme.setX((x * cell_width) + (cell_width / 2) - (width / 2));
+                        PacGomme.setY((y * cell_height) + (cell_height / 2) - (height / 2));
+                        PacGomme.setWidth(width);
+                        PacGomme.setHeight(height);
+                        PacGomme.setImageName("images/PacGum.png");
+                        PacGomme.setLayerName("BACKGROUND");
+                        pacmanWorld.addPhysicsEntity(PacGomme);
+                        pacmanWorld.addGraphicsEntity(PacGomme);
+                        break;
+                    }
+                    case 3: {
+                        double fruit_scale = 0.5;
+                        double width = cell_width * fruit_scale;
+                        double height = cell_height * fruit_scale;
+                        SuperFruit sp = new SuperFruit();
+                        sp.setX((x * cell_width) + (cell_width / 2) - (width / 2));
+                        sp.setY((y * cell_height) + (cell_height / 2) - (height / 2));
+                        sp.setWidth(width);
+                        sp.setHeight(height);
+                        sp.setImageName("images/Fruit_Cherry.png");
+                        sp.setLayerName("BACKGROUND");
+                        pacmanWorld.addPhysicsEntity(sp);
+                        pacmanWorld.addGraphicsEntity(sp);
+                        break;
+                    }
+                    case 4: {
+                        break;
+                    }
+                    case 5: {
+                        double pacman_scale = 0.99;
+                        double width = cell_width * pacman_scale;
+                        double height = cell_height * pacman_scale;
+                        pacman = new Pacman();
+                        pacman.setX((x * cell_width) + (cell_width / 2) - (width / 2));
+                        pacman.setY((y * cell_height) + (cell_height / 2) - (height / 2));
+                        pacman.setWidth(width);
+                        pacman.setHeight(height);
+                        pacman.setLayerName("FOREGROUND");
+                        pacman.setControls("Z", "S", "Q", "D");
+                        pacmanWorld.addGraphicsEntity(pacman);
+                        pacmanWorld.addPhysicsEntity(pacman);
+                        pacmanWorld.addIOEntity(pacman);
+                        break;
+                    }
+                    case 6: {
+                        double blincky_scale = 0.9;
+                        double width = cell_width * blincky_scale;
+                        double height = cell_height * blincky_scale;
+                        blinky = new Blinky();
+                        blinky.setX((x * cell_width) + (cell_width / 2) - (width / 2));
+                        blinky.setY((y * cell_height) + (cell_height / 2) - (height / 2));
+                        blinky.setWidth(width);
+                        blinky.setHeight(width);
+                        blinky.setImageName("images/Ghost_Red_1.png");
+                        blinky.setLayerName("FOREGROUND");
+                        pacmanWorld.addGraphicsEntity(blinky);
+                        pacmanWorld.addPhysicsEntity(blinky);
+                        pacmanWorld.addAIEntity(blinky);
+                        break;
+                    }
+//                    case 7: {
+//                        break;
+//                    }
+//                    case 8: {
+//                        break;
+//                    }
+//                    case 9: {
+//                        break;
+//                    }
+                    default: {
+                        System.out.println("Parsing Error");
+                    }
                 }
             }
-            System.out.println();
         }
         return mapGrid;
     }
@@ -131,13 +168,21 @@ public class PacmanApp
             throw new IllegalArgumentException("file not found! " + mapName);
         }
         String sc = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8)).lines().collect(Collectors.joining(""));
-        int[][] mapTab = new int[16][16];
+        // Map must be square
+        this.grid_size = (int) Math.sqrt(sc.length());
+        this.cell_width = Settings.WORLD_WIDTH / grid_size;
+        this.cell_height = Settings.WORLD_HEIGHT / grid_size;
 
-        System.out.println(sc.length());
+        System.out.println("Grid len= " + sc.length());
+        System.out.println("Grid size= " + grid_size);
+
+        int[][] mapTab = new int[grid_size][grid_size];
+
         for(int i = 0; i < sc.length(); i++){
             if(sc.charAt(i) != '\n')
-                mapTab[i/16][i%16] = (int) sc.charAt(i) - (int) '0';
+                mapTab[i/grid_size][i%grid_size] = (int) sc.charAt(i) - (int) '0';
         }
+
         return mapTab;
     }
 
