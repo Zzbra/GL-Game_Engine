12bdab3e7e0a18740263b86de3981e91cc521b57
Zbrazz <roux.etienne141@gmail.com>
2020-12-07 01:40:12 +0100

    Clide

--- src/main/java/amu/gl/equipe200/pacman/entities/Blinky.java
+++ src/main/java/amu/gl/equipe200/pacman/entities/Blinky.java
@@ -7,14 +7,10 @@ import java.util.ArrayList;
 
 public class Blinky extends Ghost implements IAInterface {
     Pacman pacMan;
-    ArrayList<Cell> path;
-    Cell now;
 
 
     public Blinky() {
         super();
-        this.path= new ArrayList<>();
-        this.now = new Cell((int)getX(), (int)getY(), true);
         setImageName("Images/Ghost_blue_1");
         setLayerName("FOREGROUND");
     }
