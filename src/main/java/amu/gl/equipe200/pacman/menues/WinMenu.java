a05269c5466aaa2623657a087ccd134a804e1755
Maxime <maxime.schlegel.83@gmail.com>
2020-12-07 02:14:29 +0100

    Add Menu

---
+++ src/main/java/amu/gl/equipe200/pacman/menues/WinMenu.java
@@ -0,0 +1,63 @@
+package amu.gl.equipe200.pacman.menues;
+
+import amu.gl.equipe200.core.Settings;
+import amu.gl.equipe200.graphicsengine.MenuInterface;
+import amu.gl.equipe200.pacman.PacmanApp;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+
+
+public class WinMenu
+        implements MenuInterface {
+
+    private static WinMenu INSTANCE;
+    private Scene winMenuScene;
+    private Pane root;
+    private PacmanApp linkedApp;
+    public final Button gotoMainMenu;
+
+    private WinMenu(PacmanApp app) {
+        this.linkedApp = app;
+
+        gotoMainMenu = new Button("Return To the Menu");
+        gotoMainMenu.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                linkedApp.loadMenu(MainMenu.getInstance(app));
+            }
+        });
+
+        Image img = new Image("menuScreen.jpg");
+        ImageView imgView = new ImageView(img);
+        VBox vBox = new VBox(imgView, gotoMainMenu);
+        vBox.setAlignment(Pos.BOTTOM_CENTER);
+        vBox.setSpacing(20);
+        vBox.setPadding(new Insets(50,10,50,10));
+        StackPane panel = new StackPane(vBox);
+        panel.setBackground(new Background(new BackgroundFill(Color.BLACK, CornerRadii.EMPTY, Insets.EMPTY)));
+
+        winMenuScene = new Scene(panel, Settings.SCENE_WIDTH, Settings.SCENE_HEIGHT);
+        winMenuScene.getStylesheets().add("MainMenuStyleSheet.css");
+
+        this.root = panel;
+        this.winMenuScene = winMenuScene;
+    }
+
+    public static WinMenu getInstance(PacmanApp app) {
+        if (INSTANCE == null) INSTANCE = new WinMenu(app);
+        return INSTANCE;
+    }
+
+    public Scene getScene() { return winMenuScene; }
+
+    @Override
+    public Pane getRoot() { return this.root; }
+}
